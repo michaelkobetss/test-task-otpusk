bfe0eed51fabcaa1da257b303bc6c36e71ac7bc1
Michael Kobets <michael.kobetss@gmail.com>
2025-08-31 22:08:33 +0300

    chore: make gh-pages empty

--- src/components/Layout/Layout.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/Layout/Layout.jsx
-import React from 'react'
-import styles from './Layout.module.sass'
-
-const Layout = ({ children }) => {
-    return <div className={styles.container}>{children}</div>
-}
-
-export default Layout
